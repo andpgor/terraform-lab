a097b973b607482ad7f390f2b458ab8d467ba382
Andre Gordo <andre1gordo@gmail.com>
2026-08-09 22:15:14 +0100

    working on NSG

--- main.tf
+++ main.tf
@@ -37,3 +37,49 @@ resource "azurerm_subnet" "servers" {
   virtual_network_name = azurerm_virtual_network.lab.name
   address_prefixes     = var.server_subnet
 }
+
+resource "azurerm_network_security_group" "app" {
+  name                = "nsg-app"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "app" {
+  subnet_id                 = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app.id
+}
+
+resource "azurerm_network_security_group" "servers" {
+  name                = "nsg-servers"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "servers" {
+  subnet_id                 = azurerm_subnet.servers.id
+  network_security_group_id = azurerm_network_security_group.servers.id
+}
