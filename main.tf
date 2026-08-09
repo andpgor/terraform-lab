4395d365450ccfbcc7c83813efa48fced0700871
Andre Gordo <andre1gordo@gmail.com>
2026-08-09 18:58:49 +0100

    updated terraform script

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
     }
   }
 }
@@ -15,4 +15,25 @@ provider "azurerm" {
 resource "azurerm_resource_group" "lab" {
   name     = "rg-terraform-lab-001"
   location = "uksouth"
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = "subnet-app"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_subnet" "servers" {
+  name                 = "subnet-servers"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.10.2.0/24"]
 }
