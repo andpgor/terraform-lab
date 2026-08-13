e48e1513a7bedaa4f65708de7a3ece0eb3230158
Andre Gordo <andre1gordo@gmail.com>
2026-08-13 23:16:25 +0100

    added descriptions and validations

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,23 +1,49 @@
 variable "resource_group_name" {
-  type = string
+  description = "Resource Group name"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "Azure region"
+  type        = string
 }
 
 variable "vnet_name" {
-  type = string
+  description = "Virtual Network name"
+  type        = string
+
+  validation {
+    condition     = length(var.vnet_name) > 3
+    error_message = "The VNet name must contain more than 3 characters."
+  }
 }
 
 variable "address_space" {
-  type = list(string)
+  description = "Address space for the Virtual Network"
+  type        = list(string)
+
+  validation {
+    condition     = can(cidrhost(var.address_space[0], 0))
+    error_message = "Address space must contain a valid CIDR range."
+  }
 }
 
 variable "app_subnet" {
-  type = list(string)
+  description = "Application subnet"
+  type        = list(string)
+
+  validation {
+    condition     = can(cidrhost(var.app_subnet[0], 0))
+    error_message = "Application subnet must contain a valid CIDR range."
+  }
 }
 
 variable "server_subnet" {
-  type = list(string)
+  description = "Server subnet"
+  type        = list(string)
+
+  validation {
+    condition     = can(cidrhost(var.server_subnet[0], 0))
+    error_message = "Server subnet must contain a valid CIDR range."
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -9,16 +9,16 @@ output "virtual_network_name" {
 }
 
 output "virtual_network_id" {
-  description = "Virtual Network ID"
+  description = "Resource ID of the deployed Azure Virtual Network"
   value       = module.network.virtual_network_id
 }
 
 output "app_subnet_id" {
-  description = "Application Subnet ID"
+  description = "Resource ID of the application subnet."
   value       = module.network.app_subnet_id
 }
 
 output "server_subnet_id" {
-  description = "Server Subnet ID"
+  description = "Resource ID of the server subnet."
   value       = module.network.server_subnet_id
 }
